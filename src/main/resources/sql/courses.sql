82c1c8d6344bf7ef35f512c982f9e18afb5c9f56
Themispl <themis.pilarinos@gmail.com>
2024-09-17 18:57:54 +0300

    sql files

---
+++ src/main/resources/sql/courses.sql
@@ -0,0 +1,13 @@
+use hiber6prod2db;
+
+INSERT INTO `courses` VALUES (1,'Java'),
+	(2,'C#'),
+	(3,'Python'),
+	(4,'JavaScript'),
+	(5,'TypeScript'),
+	(6,'Scala'),
+	(7,'Go'),
+	(8,'SQL'),
+	(9,'React'),
+	(10,'Angular');
+     alter table courses AUTO_INCREMENT = 11;
---
+++ src/main/resources/sql/teachers.sql
@@ -0,0 +1,6 @@
+INSERT INTO `teachers` VALUES (1,'Αθανάσιος','Ανδρούτσος',1,NULL),
+	(2,'Μάκης','Καπέτης',2,NULL),
+	(3,'Μάρκος','Καραμπάτσης',5,NULL),
+	(4,'Παναγιώτης','Μόσχος',4,NULL),
+	(5,'Σοφοκλής','Στουραϊτης',8,NULL);
+    alter table teachers AUTO_INCREMENT = 6;
